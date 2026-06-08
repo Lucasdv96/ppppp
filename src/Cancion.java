6dc285382fc7184bf8ccbadc2a43765387a6f862
lucas del valle <81876236+Lucasdv96@users.noreply.github.com>
2026-06-08 14:43:15 -0300

    creacion de clase cancion

---
+++ src/Cancion.java
@@ -0,0 +1,45 @@
+public class Cancion {
+    String nombre;
+    String fecha;
+    int duracion;
+    String genero;
+
+    public Cancion(String nombre, String fecha, int duracion, String genero) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.duracion = duracion;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
